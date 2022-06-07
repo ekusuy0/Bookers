503ba0d6e0d66a48868163ca611d83cdc5fae8ec
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-06-07 04:35:29 +0000

    [Update] bookの新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,28 @@
 class BooksController < ApplicationController
-  
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
-  
+
+  def create
+  end
+
   def show
   end
-  
+
   def edit
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
